b833a950c055cc2b355da69ea546676743c81eb8
Nexis Sis <nexis.sis@gmail.com>
2018-04-19 10:48:19 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+
+//= ../../node_modules/jquery/dist/jquery.js
+//= ../../node_modules/jcarousel/dist/jquery.jcarousel.js
+
+/*
+ * Custom
+ */
+
+//= partials/app.js
---
+++ src/js/partials/app.js
@@ -0,0 +1,30 @@
+$(function() {
+    $('.jcarousel')
+        .jcarousel({
+            wrap: 'both'
+        });
+
+    $('.jcarousel-control-prev')
+        .on('jcarouselcontrol:active', function() {
+            $(this).removeClass('inactive');
+        })
+        .on('jcarouselcontrol:inactive', function() {
+            $(this).addClass('inactive');
+        })
+        .jcarouselControl({
+            // Options go here
+            target: '-=1'
+        });
+
+    $('.jcarousel-control-next')
+        .on('jcarouselcontrol:active', function() {
+            $(this).removeClass('inactive');
+        })
+        .on('jcarouselcontrol:inactive', function() {
+            $(this).addClass('inactive');
+        })
+        .jcarouselControl({
+            // Options go here
+            target: '+=1'
+        });
+});
