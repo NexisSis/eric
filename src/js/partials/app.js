d3cb47363afedbc6bcba363e57215c8430101e26
Nexis Sis <nexis.sis@gmail.com>
2018-05-15 17:56:11 +0300

    mobile version home page

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,3 +1,4 @@
+// honors carousel init
 $(function() {
     $('.jcarousel')
         .jcarousel({
@@ -29,7 +30,37 @@ $(function() {
         });
 });
 
+//testimonials carousele init
+$(function() {
+    $('.testimonials-carousel')
+        .jcarousel({
+            wrap: 'both'
+        });
 
+    $('.testimonials-control-back')
+        .on('jcarouselcontrol:active', function() {
+            $(this).removeClass('inactive');
+        })
+        .on('jcarouselcontrol:inactive', function() {
+            $(this).addClass('inactive');
+        })
+        .jcarouselControl({
+            // Options go here
+            target: '-=1'
+        });
+
+    $('.testimonials-control-next')
+        .on('jcarouselcontrol:active', function() {
+            $(this).removeClass('inactive');
+        })
+        .on('jcarouselcontrol:inactive', function() {
+            $(this).addClass('inactive');
+        })
+        .jcarouselControl({
+            // Options go here
+            target: '+=1'
+        })
+});
 new WOW().init();
 //header animation
 window.onscroll = function() {scrollFix()};
