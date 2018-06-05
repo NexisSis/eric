b7b441c23ff667d34bb66784ea3fb8ecd42240db
Nexis Sis <nexis.sis@gmail.com>
2018-06-05 18:59:31 +0300

    rebuild innerpage

--- sendmail.php
+++ sendmail.php
@@ -1,3 +1,16 @@
 <?php
+if(isset($_POST['submit'])){
+    $to = "email@example.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sendr's Email address
+    $first_name = $_POST['name'];
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
 
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
 ?>
