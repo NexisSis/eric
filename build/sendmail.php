4f0d022caf564c875b877fba507956e779900d87
NexisSis <Nexis.sis@gmail.com>
2018-06-10 19:03:23 +0300

    change mail config

--- build/sendmail.php
+++ build/sendmail.php
@@ -34,17 +34,10 @@ if(isset($_POST['submit'])){
             echo "System Error. ".$error;
         }
 
-
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    }
-?>
-
-
-<?php
-    $SmtpServer="mail.artkiev.com";
+    $SmtpServer="mail.mausnerlawfirm.com";
     $SmtpPort="25";
-    $SmtpUser="username";
-    $SmtpPass="password";
+    $SmtpUser="eric@mausnerlawfirm.com";
+    $SmtpPass="Fartyuiop1";
     class SMTPClient
     {
         function SMTPClient ($SmtpServer, $SmtpPort, $SmtpUser, $SmtpPass, $from, $to, $subject, $body)
@@ -91,13 +84,74 @@ if(isset($_POST['submit'])){
             return $talk;
             }
         }
-    if($_SERVER["REQUEST_METHOD"] == "POST")
+            $to = $_POST['to'];
+            $from = $_POST['from'];
+            $subject = $_POST['sub'];
+            $body = $_POST['message'];Ж
+            $SMTPMail = new SMTPClient ($SmtpServer, $SmtpPort, $SmtpUser, $SmtpPass, $from, $to, $subject, $body);
+            $SMTPChat = $SMTPMail->SendMail();
+
+
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+?>
+
+
+<?php
+    $SmtpServer="mail.mausnerlawfirm.com";
+    $SmtpPort="25";
+    $SmtpUser="eric@mausnerlawfirm.com";
+    $SmtpPass="Fartyuiop1";
+    class SMTPClient
     {
+        function SMTPClient ($SmtpServer, $SmtpPort, $SmtpUser, $SmtpPass, $from, $to, $subject, $body)
+        {
+            $this->SmtpServer = $SmtpServer;
+            $this->SmtpUser = base64_encode ($SmtpUser);
+            $this->SmtpPass = base64_encode ($SmtpPass);
+            $this->from = $from;
+            $this->to = $to;
+            $this->subject = $subject;
+            $this->body = $body;
+            if ($SmtpPort == "")
+            {
+                $this->PortSMTP = 25;
+            }
+            else
+            {
+                $this->PortSMTP = $SmtpPort;
+            }
+        }
+            function SendMail ()
+            {
+                if ($SMTPIN = fsockopen ($this->SmtpServer, $this->PortSMTP))
+                {
+                    fputs ($SMTPIN, "EHLO ".$HTTP_HOST."\r\n");
+                    $talk["hello"] = fgets ( $SMTPIN, 1024 );
+                    fputs($SMTPIN, "auth login\r\n");
+                    $talk["res"]=fgets($SMTPIN,1024);
+                    fputs($SMTPIN, $this->SmtpUser."\r\n");
+                    $talk["user"]=fgets($SMTPIN,1024);
+                    fputs($SMTPIN, $this->SmtpPass."\r\n");
+                    $talk["pass"]=fgets($SMTPIN,256);
+                    fputs ($SMTPIN, "MAIL FROM: <".$this->from.">\r\n");
+                    $talk["From"] = fgets ( $SMTPIN, 1024 );
+                    fputs ($SMTPIN, "RCPT TO: <".$this->to.">\r\n");
+                    $talk["To"] = fgets ($SMTPIN, 1024);
+                    fputs($SMTPIN, "DATA\r\n");
+                    $talk["data"]=fgets( $SMTPIN,1024 );
+                    fputs($SMTPIN, "To: <".$this->to.">\r\nFrom: <".$this->from.">\r\nSubject:".$this->subject."\r\n\r\n\r\n".$this->body."\r\n.\r\n");
+                    $talk["send"]=fgets($SMTPIN,256);
+                    fputs ($SMTPIN, "QUIT\r\n");
+                    fclose($SMTPIN);
+                }
+            return $talk;
+            }
+        }
         $to = $_POST['to'];
         $from = $_POST['from'];
         $subject = $_POST['sub'];
         $body = $_POST['message'];Ж
         $SMTPMail = new SMTPClient ($SmtpServer, $SmtpPort, $SmtpUser, $SmtpPass, $from, $to, $subject, $body);
         $SMTPChat = $SMTPMail->SendMail();
-    }
 ?>
